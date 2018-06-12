0179e50f8196c928342723927ebbf915750848da
Tobias Hvass Mølbak <thm@combine.dk>
2018-06-12 12:40:30 +0200

    Added imageactivity

---
+++ app/src/main/java/dk/molbak/kotlintestproject/ImageActivity.kt
@@ -0,0 +1,13 @@
+package dk.molbak.kotlintestproject
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+
+class ImageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_image)
+    }
+}
